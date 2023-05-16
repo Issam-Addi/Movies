755c5a65ddf963a3e2ef01f34a9ae07dce5d9dc5
Issam-Addi <essam.h.addi@gmail.com>
2023-05-16 11:19:15 +0300

    noed

--- first-back-end/src/App.js
+++ first-back-end/src/App.js
@@ -15,17 +15,24 @@ const MoviesList = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Movies List</h1>
-      {movies.map(movie => (
-        <div key={movie.id} className='cont'>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
-          <h2>{movie.title}</h2>
-          <p>{movie.overview}</p>
-          <hr/>
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold my-10">Movies List</h1>
+      {movies.map((movie) => (
+        <div key={movie.id} className="bg-white rounded shadow-md p-4 mb-4 w-full max-w-xl">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            className="w-full mb-4"
+            alt={movie.title}
+          />
+          <h1 className="text-xl font-bold mb-2">Name of movie: {movie.title}</h1>
+          <p className="text-gray-600 mb-2">
+            <span className="font-bold">Movie description:</span> {movie.overview}
+          </p>
+          <hr className="border-gray-300 my-4" />
         </div>
       ))}
     </div>
+
   );
 };
 
